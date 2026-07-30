120464b6871bb7aaa3a65ba6233980e6a4768f17
MeterReader <meterreader@example.com>
2026-07-31 00:13:27 +0800

    fix: 添加 JitPack 仓库以解决 MPAndroidChart 下载失败

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,11 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
